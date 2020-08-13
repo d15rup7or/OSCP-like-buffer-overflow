cda5fb0fb9eb6da08233fed8af73fcfe2b30e784
d15rup7or <32622016+d15rup7or@users.noreply.github.com>
2020-08-13 10:22:48 +0200

    Create fuzzer.py

---
+++ fuzzer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import socket
+
+RHOST = "192.168.56.101"  # mind to change :-)
+RPORT = "31337"           # mind to change :-)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST, RPORT))
+
+buf = ""
+buf += "A"*1024
+buf += "\n"
+
+s.send(buf)
