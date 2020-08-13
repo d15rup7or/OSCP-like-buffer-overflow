75c8bd31fc2a460a082d709a27e0da44ce2734d3
d15rup7or <32622016+d15rup7or@users.noreply.github.com>
2020-08-13 17:43:19 +0200

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python python2.7.18
 import socket
 
-RHOST = "192.168.56.101"  # mind to change :-)
-RPORT = "31337"           # mind to change :-)
+RHOST = "192.168.56.101"
+RPORT = 31337
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST, RPORT))
